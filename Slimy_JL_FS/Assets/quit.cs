be19fcc4c1e0bbae09f364e3d25cdb9d87d438d3
Finn <91551990+Finnlandish@users.noreply.github.com>
2023-05-25 17:12:12 -0400

    .

---
+++ Slimy_JL_FS/Assets/quit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+    }
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+}
