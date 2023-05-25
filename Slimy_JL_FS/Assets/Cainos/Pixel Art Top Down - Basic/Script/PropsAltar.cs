c4abe2246c4be88d17bb7d431aeadb1175eb89fa
jslongtin <jessikalongtin@hotmail.com>
2023-05-25 16:29:05 -0400

    progress niv 2

--- Slimy_JL_FS/Assets/Cainos/Pixel Art Top Down - Basic/Script/PropsAltar.cs
+++ Slimy_JL_FS/Assets/Cainos/Pixel Art Top Down - Basic/Script/PropsAltar.cs
@@ -9,28 +9,19 @@ namespace Cainos.PixelArtTopDown_Basic
     public class PropsAltar : MonoBehaviour
     {
         public List<SpriteRenderer> runes;
-        public float lerpSpeed;
 
-        private Color curColor;
-        private Color targetColor;
+      
+        [SerializeField]
+        private Color targetColor = new Color(255,255,255,255);
 
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            targetColor = new Color(1, 1, 1, 1);
-        }
-
-        private void OnTriggerExit2D(Collider2D other)
-        {
-            targetColor = new Color(1, 1, 1, 0);
-        }
-
+  
         private void Update()
         {
-            curColor = Color.Lerp(curColor, targetColor, lerpSpeed * Time.deltaTime);
+         
 
             foreach (var r in runes)
             {
-                r.color = curColor;
+                r.color = targetColor;
             }
         }
     }
